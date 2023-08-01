39b724fbaef530dd1e5c4e2baf8b4ac399b71d10
N0tAdm1n <n0tadm1n29@gmail.com>
2023-08-01 23:57:57 +0530

    Add id using nanoid

--- src/helper/getQuestions.js
+++ src/helper/getQuestions.js
@@ -1,3 +1,4 @@
+import { nanoid } from "nanoid";
 import getDataFromAPI from "./getDataFromAPI";
 
 export default async function getQAObject() {
@@ -13,6 +14,7 @@ export default async function getQAObject() {
       choicesArray,
       choosenAnswer: "",
       isCorrect: false,
+      id: nanoid(),
     };
   });
 
