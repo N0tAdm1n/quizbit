fbf157371349b7e18167ff8bd15409434aef9936
N0tAdm1n <64731692+N0tAdm1n@users.noreply.github.com>
2023-08-01 16:59:26 +0530

    Set state of questionsArr on startup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import getQAObject from "./helper/getQuestions";
 
 function App() {
-	const [questionsArr, setQuestionsArr] = useState(getQAObject());
+	const [questionsArr, setQuestionsArr] = useState([]);
 
 	async function handleClick() {
 		console.log(questionsArr);
 	}
+
+	async function getNewQuestions() {
+		try {
+			const data = await getQAObject();
+			setQuestionsArr(data);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+	useEffect(() => {
+		getNewQuestions();
+	}, []);
 
 	return (
 		<>
