9692f1bd396c065906f61fc2e4c3ad79e1996a6a
N0tAdm1n <64731692+N0tAdm1n@users.noreply.github.com>
2023-08-01 16:55:35 +0530

    Remov console logs

--- src/helper/getQuestions.js
+++ src/helper/getQuestions.js
@@ -2,11 +2,9 @@ import getDataFromAPI from "./getDataFromAPI";
 
 export default async function getQAObject() {
 	let result = await getDataFromAPI();
-	console.log(result);
 	const qaObject = result.map((ele) => {
 		const question = ele.question;
 		const correctAnswer = ele.correct_answer;
-		console.log(ele.incorrect_answers);
 		const choicesArray = [correctAnswer, ...ele.incorrect_answers];
 
 		return {
