4e031d6209d5af9d92ede2b2d8e0385bd995b116
N0tAdm1n <64731692+N0tAdm1n@users.noreply.github.com>
2023-08-08 16:23:43 +0530

    Shuffle questions

--- src/helper/getQuestions.js
+++ src/helper/getQuestions.js
@@ -6,7 +6,7 @@ export default async function getQAObject() {
 	const qaObject = result.map((ele) => {
 		const question = ele.question;
 		const correctAnswer = ele.correct_answer;
-		const answerArray = [correctAnswer, ...ele.incorrect_answers];
+		const answerArray = shuffle([correctAnswer, ...ele.incorrect_answers]);
 		const choicesArray = answerArray.map((answer) => {
 			return {
 				answer: answer,
@@ -25,4 +25,24 @@ export default async function getQAObject() {
 	});
 
 	return qaObject;
+}
+
+function shuffle(array) {
+	let currentIndex = array.length,
+		randomIndex;
+
+	// While there remain elements to shuffle.
+	while (currentIndex != 0) {
+		// Pick a remaining element.
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+
+		// And swap it with the current element.
+		[array[currentIndex], array[randomIndex]] = [
+			array[randomIndex],
+			array[currentIndex],
+		];
+	}
+
+	return array;
 }
